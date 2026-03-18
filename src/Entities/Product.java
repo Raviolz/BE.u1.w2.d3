f3e8be862a8736c78507c142d64d78ea5ec65e78
Raviolz <gi.ravioli@gmail.com>
2026-03-18 14:37:18 +0100

    ClassesNMethods

---
+++ src/Entities/Product.java
@@ -0,0 +1,41 @@
+package Entities;
+
+public class Product {
+
+    // attributi
+
+    private long id;
+    private String name;
+    private String category;
+    private double price;
+
+    // costruttore
+
+    public Product(long idInput, String nameInput, String categoryInput, double priceInput) {
+        this.id = idInput;
+        this.name = nameInput;
+        this.category = categoryInput;
+        this.price = priceInput;
+
+    }
+
+
+    // metodi get
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
